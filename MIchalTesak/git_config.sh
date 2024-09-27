20133992976df8e33a01eb19b5b2adaf647cc12e
00CzechNinja <159769478+00CzechNinja@users.noreply.github.com>
2024-09-27 07:43:38 +0200

    Add files via upload

---
+++ MIchalTesak/git_config.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+echo "Welcome to Git config"
+git config --global user.email "tesakmisa@gmail.com"
+git config --global user.name "Michal Tesák"
+git config --list
+echo "Done Successfully"
